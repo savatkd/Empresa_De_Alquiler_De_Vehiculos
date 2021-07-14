4ad0fb948f6330f32ed556d4c4bdbe4945d53f48
Edwin Rivadeneira <76056809+savatkd@users.noreply.github.com>
2021-07-13 21:16:41 -0500

    Actualizacion

--- Empresa de alquiler de Vehículos/Consulta #1.sql
+++ Empresa de alquiler de Vehículos/Consulta #1.sql
@@ -1,8 +1,8 @@
 /* por cada mes cuanto es el total de facturas que se han generado y el dinero que se ha ingresado */
 
 
-select top (5) NOMBRE_EMPRESA, TIPO_SERVICIO, FECHA_FACTURA ,  sum (TOTAL_FACTURA*IVA_FACTURA) as Ingreso_Mes  
+select top (5) NOMBRE_EMPRESA, TIPO_SERVICIO, FECHA_FACTURA ,NUMERO_FACTURA,  sum (TOTAL_FACTURA*IVA_FACTURA) as Ingreso_Mes  
 from  EMPRESA, SERVICIO  inner join FACTURA on SERVICIO.ID_SERVICIO=FACTURA.ID_FACTURA 
 where SERVICIO.ID_SERVICIO=FACTURA.ID_FACTURA
-group by NOMBRE_EMPRESA, TIPO_SERVICIO, FECHA_FACTURA
+group by NOMBRE_EMPRESA, TIPO_SERVICIO, FECHA_FACTURA, NUMERO_FACTURA
 order by Ingreso_Mes;
